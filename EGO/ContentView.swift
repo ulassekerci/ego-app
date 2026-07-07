fee73c701cf2bb782c23164c826a5763021e59e4
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2026-07-08 02:21:39 +0300

    Initial Commit

---
+++ EGO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EGO
+//
+//  Created by ulassekerci on 08/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EGO/EGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGOApp.swift
+//  EGO
+//
+//  Created by ulassekerci on 08/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
